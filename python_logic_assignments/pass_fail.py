1013283f56b518d81cabd7747ce1565b8de41385
Ritish1208 <knowjavabluej@gmail.com>
2026-02-08 21:57:46 +0530

    Added input validation for score

--- python_logic_assignments/pass_fail.py
+++ python_logic_assignments/pass_fail.py
@@ -1,6 +1,8 @@
-score = int(input("Enter the score"))
-if score >=50 :
-    print("Pass")
-else :
-    print("Fail")
-    
+try:
+    score = int(input("Enter the score: "))
+    if score >=50 :
+       print("Pass")
+    else :
+       print("Fail")
+except ValueError:
+   print("Invalid input, Please enter an integer score")
