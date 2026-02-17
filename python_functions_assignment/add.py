baaffa81f07b3180a516ebf7ef2add73be13d4e2
Ritish1208 <knowjavabluej@gmail.com>
2026-02-17 23:10:13 +0530

    Part 2 - Adding 2 numbers function  completed and added

---
+++ python_functions_assignment/add.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+#Take input from user
+num1= int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+# call the function with user input
+sum = add(num1,num2)
+#Print the result
+print(f"Sum is: {sum}")
